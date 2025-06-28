d61af7d48df7d4a92fd0023381478c0799f63a5b
BhanudasRane1 <ranebhanudas786@gmail.com>
2025-06-28 19:14:33 +0530

    Implemented getProducts API,Using H2 DB, Data JPA, Lombok and DTO pattern

--- eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,10 +2,16 @@ package com.eazybytes.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication()
+//@EnableJpaRepositories
+//@EntityScan
 public class EazystoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EazystoreApplication.class, args);
 	}
