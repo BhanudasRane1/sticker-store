b97458037a4f30c9cce3b8ed88126c8075825659
BhanudasRane1 <ranebhanudas786@gmail.com>
2025-07-22 10:02:41 +0530

    CSRF validation added

--- eazystore-ui/src/api/apiClient.js
+++ eazystore-ui/src/api/apiClient.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const VITE_API_BASE_URL = "http://localhost:8080/api/v1";
+import Cookies from "js-cookie";
+
 const apiClient = axios.create({
   baseURL: VITE_API_BASE_URL,
   headers: {
@@ -8,6 +10,7 @@ const apiClient = axios.create({
     Accept: "application/json",
   },
   timeout: 10000,
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use(
@@ -16,6 +19,22 @@ apiClient.interceptors.request.use(
     if (jwtToken) {
       config.headers.Authorization = `Bearer ${jwtToken}`;
     }
+    // Only fetch CSRF token for non-safe methods
+    const safeMethods = ["GET", "HEAD", "OPTIONS"];
+    if (!safeMethods.includes(config.method.toUpperCase())) {
+      let csrfToken = Cookies.get("XSRF-TOKEN");
+      if (!csrfToken) {
+        await axios.get(`${VITE_API_BASE_URL}/csrf-token`, {
+          withCredentials: true,
+        });
+        csrfToken = Cookies.get("XSRF-TOKEN");
+        if (!csrfToken) {
+          throw new Error("Failed to retrieve CSRF token from cookies");
+        }
+      }
+      //config.headers["X-XSRF-TOKEN"] = csrfToken;
+    }
+
     return config;
   },
   (error) => Promise.reject(error)
